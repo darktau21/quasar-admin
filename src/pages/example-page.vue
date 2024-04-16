7739fd3e16d4f0ba204847087db325ce35db3bce
Kirill Naumenko <naumenkokirill466@gmail.com>
2024-04-16 23:55:49 +0300

    refactor: изменена структура проекта

---
+++ src/pages/example-page.vue
@@ -0,0 +1,7 @@
+<template>I am example page</template>
+<script setup lang="ts">
+    const aboba = {
+        bar: true,
+        foo: false,
+    };
+</script>
---
+++ src/shared/lib/prisma/index.ts
@@ -0,0 +1,8 @@
+const test = {
+    aboba: false,
+    down: true,
+    eboba: 'fds',
+};
+
+const t: number = 'aboba';
+console.log(t);
