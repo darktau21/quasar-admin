71e2b32ab6edded0f10e243e2e7e7642154bd3ab
Kirill Naumenko <naumenkokirill466@gmail.com>
2024-05-31 17:04:06 +0300

    fix: ограничения инпутов

--- src/shared/schemas.ts
+++ src/shared/schemas.ts
@@ -16,24 +16,24 @@ export const News = object({
     title: z
         .string({ message: 'Обязательное поле' })
         .min(2, 'Минимальная длинна заголовка - 2')
-        .max(80, 'Максимальная длинна заголовка - 80'),
+        .max(37, 'Максимальная длинна заголовка - 37'),
 });
 
 export const Teammate = object({
     description: z
         .string({ message: 'Обязательное поле' })
         .min(2, 'Минимальная длинна - 2')
-        .max(300, 'Максимальная длинна - 300'),
+        .max(440, 'Максимальная длинна - 440'),
     id: z.number().optional(),
     imageUrl: z.string().optional(),
     name: z
         .string({ message: 'Обязательное поле' })
         .min(2, 'Минимальная длинна - 2')
-        .max(128, 'Максимальная длинна - 128'),
+        .max(90, 'Максимальная длинна - 90'),
     quote: z
         .string()
         .min(2, 'Минимальная длинна - 2')
-        .max(128, 'Максимальная длинна - 128')
+        .max(60, 'Максимальная длинна - 60')
         .optional(),
     tags: z.array(z.string()).optional(),
 });
@@ -42,7 +42,7 @@ export const Winner = object({
     description: z
         .string({ message: 'Обязательное поле' })
         .min(2, 'Минимальная длинна - 2')
-        .max(300, 'Максимальная длинна - 300'),
+        .max(100, 'Максимальная длинна - 100'),
     id: z.number().optional(),
     imageUrl: z.string().optional(),
     medals: z
@@ -53,7 +53,7 @@ export const Winner = object({
     name: z
         .string({ message: 'Обязательное поле' })
         .min(2, 'Минимальная длинна - 2')
-        .max(128, 'Максимальная длинна - 128'),
+        .max(30, 'Максимальная длинна - 30'),
     review: z
         .string()
         .min(2, 'Минимальная длинна - 2')
